025158ec28103e845492243588e39827d1582e71
Kevin Lubick <kjlubick@google.com>
2016-04-21 10:13:19 -0400

    Fix booting problem

--- format_new_32G_card.sh
+++ format_new_32G_card.sh
@@ -2,7 +2,21 @@
  
 DISK=/dev/sdb
 
+sudo umount $DISK"1"
+sudo umount $DISK"2"
+sudo umount $DISK"3"
+sudo umount $DISK"4"
+sudo umount $DISK"5"
+sudo umount $DISK"6"
+sudo umount $DISK"7"
 sudo sfdisk $DISK < ./32Gpartitions
+
+sudo mkfs.fat $DISK"1" -n BOOT
+sudo mkfs.ext4  $DISK"3" -L B
+sudo mkfs.ext4  $DISK"5" -L VAR
+sudo mkfs.ext4  $DISK"6" -L TMP
+sudo mkfs.ext4  $DISK"7" -L HOME
+
 sudo umount $DISK"1"
 sudo umount $DISK"2"
 sudo umount $DISK"3"
@@ -10,9 +24,7 @@ sudo umount $DISK"5"
 sudo umount $DISK"6"
 sudo umount $DISK"7"
 
-sudo mkfs.fat $DISK"1" -n BOOT
-sudo mkfs.ext4  $DISK"3" -L B
-sudo mkfs.ext4  $DISK"5" -L VAR
-sudo mkfs.ext4  $DISK"6" -L TMP
-sudo mkfs.ext4  $DISK"7" -L HOME
+sudo mount $DISK"1" /media/boot
+sudo cp -rp /opt/raspberrypi/boot/* /media/boot/
+sudo umount $DISK"1"
 
